d10431da7c4e9bf7bfb9eab2dc84ff7590c2f8ad
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-10-16 19:19:26 +0100

    Add getters for visitor

--- src/main/java/themepark/Visitor.java
+++ src/main/java/themepark/Visitor.java
@@ -1,4 +1,25 @@
 package themepark;
 
 public class Visitor {
+    int age;
+    double height;
+    double money;
+
+    public Visitor(int age, double height, double money) {
+        this.age = age;
+        this.height = height;
+        this.money = money;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getMoney() {
+        return money;
+    }
 }
