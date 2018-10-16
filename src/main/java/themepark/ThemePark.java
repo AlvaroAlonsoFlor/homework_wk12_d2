0e3cc72aab12c8caaf7329337bc05076a7edcf85
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-10-16 20:49:06 +0100

    Add getAllReviewed method to ThemePark

--- src/main/java/themepark/ThemePark.java
+++ src/main/java/themepark/ThemePark.java
@@ -1,4 +1,19 @@
 package themepark;
 
+import interfaces.IReviewed;
+
+
+import java.util.ArrayList;
+
 public class ThemePark {
+    private ArrayList<IReviewed> reviewedPlaces;
+
+    public ThemePark() {
+        this.reviewedPlaces = reviewedPlaces;
+    }
+
+    public ArrayList<IReviewed> getAllReviewed() {
+        return reviewedPlaces;
+    }
+
 }
